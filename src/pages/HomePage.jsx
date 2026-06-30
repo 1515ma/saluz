a799bae3ee84190f3bc8678ba2e2d1d723f017a0
Marcos gabriel <137959774+1515ma@users.noreply.github.com>
2026-06-30 20:11:48 -0300

    Remove LogoCustomizer component from HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -21,7 +21,6 @@ export default function HomePage() {
       <Hero />
       <About />
       <Products />
-      <LogoCustomizer />
       <Process />
       <Testimonials />
       <RecentPosts />
